696c7a5e4b5c01432d1c21b1420b8a1aa0d4f53d
conradkoh <conradkoh@gmail.com>
2026-01-04 00:38:07 +0800

    add Bun server serving static HTML page

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import index from "./index.html";
+
+Bun.serve({
+  port: 9176,
+  routes: {
+    "/": index,
+  },
+  development: {
+    hmr: true,
+    console: true,
+  },
+});
+
+console.log("Server running at http://localhost:9176");
